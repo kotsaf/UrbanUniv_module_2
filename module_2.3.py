9c470944e0d5c5776ad6c42c548f55b986f295f5
kotsaf <safqwer.136@gmail.com>
2024-08-06 21:48:41 +0300

    Add files via upload

---
+++ module_2.3.py
@@ -0,0 +1,9 @@
+list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while True:
+    if list[index] > 0:
+        print(list[index])
+    index += 1
+    if list[index] <0:
+        break
+    continue
